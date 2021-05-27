c3cbed7715c4d9675a514b45017222d8b84827c8
Dora <dorothyz@princeton.edu>
2021-05-27 19:53:04 -0400

    initial commit

---
+++ .cache/caches/gatsby-plugin-mdx/mdx-scopes-dir/85add4b20f47a83c98fdb42cecfd26d4.js
@@ -0,0 +1,6 @@
+import SpotifyPlayer from "../../../../content/posts/fantastic-beasts-and-where-to-find-them/SpotifyPlayer";
+import * as React from 'react';
+export default {
+  SpotifyPlayer,
+  React
+};
