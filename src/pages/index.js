e0d6e03c9a8c475a7839c98fb513dbece7535f75
Dora <dorothyz@princeton.edu>
2021-05-27 22:32:34 -0400

    add profile picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,8 @@ import * as React from "react"
 import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
+import css from "./index.css"
+import { StaticImage } from "gatsby-plugin-image"
 
 const LandingPage = ({ data, location }) => {
   const siteTitle = ''
@@ -9,16 +11,31 @@ const LandingPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <div>
-        <h2> Hi 👋, I'm Dora Zhao. </h2>
+        <div className="hero">
+          <div className="profile-pic">
+            <StaticImage
+              className="bio-avatar"
+              layout="fixed"
+              formats={["AUTO", "WEBP", "AVIF"]}
+              src="../images/headshot.png"
+              width={200}
+              height={200}
+              quality={95}
+              alt="Profile picture"
+            />
+          </div>
+          <div>
+            <h2> Hi 👋, I'm Dora Zhao. </h2>
+            <body>
+              I am an incoming Master's student at Princeton University, studying
+              computer science. I am interested in machine learning fairness, AI ethics, and
+              human-computer interaction. Currently, I am working in the <a href="http://visualai.princeton.edu/" target="_blank">
+              Princeton Visual AI Lab</a>, advised by <a href="https://www.cs.princeton.edu/~olgarus/" target="_blank">Professor Olga Russakovsky</a>.
+              This summer, I am interning at Sony AI in the AI Ethics group.
+            </body>
+          </div>
+        </div>
         <body>
-          I am an incoming Master's student at Princeton University, studying
-          computer science. I am interested in machine learning fairness, AI ethics, and
-          human-computer interaction. Currently, I am working in the <a href="http://visualai.princeton.edu/" target="_blank">
-          Princeton Visual AI Lab</a>, advised by <a href="https://www.cs.princeton.edu/~olgarus/" target="_blank">Professor Olga Russakovsky</a>.
-          This summer, I am interning at Sony AI in the AI Ethics group.
-
-          <br/>
-          <br/>
           I received my undergraduate degree from Princeton University, where I majored
           in computer science with minors in Asian American Studies and statistics + machine learning.
           Previously, I have worked in the Stigma and Social Perception Lab led by Professors <a href="https://psych.princeton.edu/person/nicole-shelton" target="_blank">J. Nicole Shelton</a> and <a href="https://psych.princeton.edu/person/stacey-sinclair" target="_blank">Stacey Sinclair</a>,
@@ -27,9 +44,9 @@ const LandingPage = ({ data, location }) => {
         </body>
         <br/>
         <div>
-        <a href="mailto:dorothyz@princeton.edu">[email]</a>
-        &emsp; <a href="https://github.com/dorazhao99/" target="_blank">[github]</a>
-        &emsp; <a href="https://www.linkedin.com/in/dorothy-z/" target="_blank">[linkedin]</a>
+          <a href="mailto:dorothyz@princeton.edu">[email]</a>
+          &emsp; <a href="https://github.com/dorazhao99/" target="_blank">[github]</a>
+          &emsp; <a href="https://www.linkedin.com/in/dorothy-z/" target="_blank">[linkedin]</a>
         </div>
       </div>
       <div>
