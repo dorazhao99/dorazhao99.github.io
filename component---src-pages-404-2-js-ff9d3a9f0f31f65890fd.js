b29ab3bb5bc65445f7216c636ae798442dd54830
dorazhao99 <dorothyz@princeton.edu>
2023-05-01 20:03:20 -0400

    Updates

---
+++ component---src-pages-404-2-js-ff9d3a9f0f31f65890fd.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_blog=self.webpackChunkgatsby_starter_blog||[]).push([[12],{9576:function(t,e,a){a.r(e);var l=a(7294),n=a(8678),s=a(8183);e.default=t=>{let{data:e,location:a}=t;const o=e.site.siteMetadata.title;return l.createElement(n.Z,{location:a,title:o},l.createElement(s.Z,{title:"404: Not Found"}),l.createElement("h1",null,"404: Not Found"),l.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-2-js-ff9d3a9f0f31f65890fd.js.map
